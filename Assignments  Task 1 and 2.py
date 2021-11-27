5d09b38cd7a6d0e4ba7d66caddc39b746feeeb2b
Siddho <87514648+Siddho@users.noreply.github.com>
2021-11-27 09:10:30 +0530

    Add files via upload

---
+++ Assignments  Task 1 and 2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Assignemnet-1
+#Circle
+from math import pi
+radius=float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + " is: " + str(pi * radius**2))
+
+
+# In[2]:
+
+
+#File name Extension
+Filename=input("Enter the Filename: ")
+f_extns = Filename.split(".")
+print ("The extension of the File is : " + repr(f_extns[-1]))
+
+
+# In[ ]:
+
+
+
+
