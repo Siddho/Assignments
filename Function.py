46a2a8ae837b4208e2d967a0e37a3baae12665c8
Siddho <87514648+Siddho@users.noreply.github.com>
2021-11-27 20:51:33 +0530

    Add files via upload
    Function Assignment

---
+++ Function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Assignment
+#Functions Task
+Str= input('Please enter a string ')
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] -= 1
+    return d
+
+print (most_frequent(Str))
+
