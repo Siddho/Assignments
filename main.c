3d8d2d83206c2569571e84b4f033b31dfb7cdcba
Siddho <87514648+Siddho@users.noreply.github.com>
2022-01-26 20:53:32 +0530

    pattern loop

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
-int lcm(int a,int b);
-int main()
-{
-    int num1, num2, LCM;
-    printf("Enter any two numbers to find LCM: ");
-    scanf("%d%D", &num1, &num2);
-    if (num1 > num2)
-        LCM = lcm(num2, num1);
-    else
-        LCM = lcm(num1, num2);
-    printf("LCM of %d and %d = %d", num1, num2, LCM);
+
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for (int j = 0; j >=1 ;j--)
+        {
+        printf("%d",j);
+
+        }
+        printf("\n");
+    }
     return 0;
-}
-int lcm(int a ,int b)
-{
-    static int multiple =0;
-    if((multiple % a ==0) && (multiple % b ==0))
-    {
-        return multiple;
-
-    }
-    else
-    {
-        return lcm(a,b);
-    }
-
 }
