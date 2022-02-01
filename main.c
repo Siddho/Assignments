e01b09dcf07a2d38f42307d04ec2601cd66d124a
Siddho <87514648+Siddho@users.noreply.github.com>
2022-02-01 21:58:41 +0530

    Data Structures

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    int i,j;
-    for(i=1;i<=5;i++)
-    {
-        for (int j = 0; j >=1 ;j--)
-        {
-        printf("%d",j);
+    struct employee {
+        char name[30];
+        int empId;
+        float salary;
+    };
+int main()
+{
+    struct employee emp;
+    printf("\nEnter details :\n");
+    printf("Name?:");
+    gets(emp.name);
+    printf("ID?:");
+    scanf("%d",&emp.empId);
+    printf("Salary ?:");
+    scanf("%f",&emp.salary);
 
-        }
-        printf("\n");
-    }
+    printf("\nEntered details :\n");
+    printf(("Name:%s" ,emp.name));
+    printf("Id:%d" ,emp.empId);
+    printf("Salary:%f\n",emp.salary);
     return 0;
+
+
+
 }
+
